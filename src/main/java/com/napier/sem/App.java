6e4eb4afb19c4aba8575fa27108224f300893cd5
JoshM <6joshmendez@gmail.com>
2023-02-28 18:18:42 +0000

    2

---
+++ src/main/java/com/napier/sem/App.java
@@ -0,0 +1,13 @@
+package com.napier.sem;
+/*
+Test java class
+ */
+public class App
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Coursework test");//print test to see if class is working
+    }
+}
+
+
